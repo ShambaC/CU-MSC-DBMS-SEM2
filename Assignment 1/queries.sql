45f831a3c9171346060c0399604f0cd089457261
ShambaC <38806897+ShambaC@users.noreply.github.com>
2024-05-16 10:13:18 +0530

    Added queries for the assignment

---
+++ Assignment 1/queries.sql
@@ -0,0 +1,23 @@
+select product_no, description, sell_price from product_master where sell_price > 500 && sell_price < 750;
+
+select order_no, order_date from sales_order where month(order_date) = 5 || month(order_date) = 6;
+
+select product_no, description, qty_ordered from product_master where product_no in (
+        select product_no, qty_ordered from sales_order_details where order_no in (
+        select order_no from sales_order where client_no in (
+            select client_no from client_master where name like 'Ravi' || name like 'Basu'
+        )
+    )
+);
+
+select
+    p.product_no,
+    p.description,
+    qty_ordered
+from
+    product_master p
+    join sales_order_details sod on p.product_no = sod.product_no
+    join sales_order so on sod.order_no = so.order_no
+    join client_master cm on so.client_no = cm.client_no
+where
+    cm.name in ('Raj', 'Basu');
