6cd5f15bcb4a56d7a0ffa82caeafbf8c84ed215e
ShambaC <38806897+ShambaC@users.noreply.github.com>
2024-05-17 17:37:04 +0530

    Removed a unneeded query

--- Assignment 1/queries.sql
+++ Assignment 1/queries.sql
@@ -2,14 +2,6 @@ select product_no, description, sell_price from product_master where sell_price 
 
 select order_no, order_date from sales_order where month(order_date) = 5 || month(order_date) = 6;
 
-select product_no, description, qty_ordered from product_master where product_no in (
-        select product_no, qty_ordered from sales_order_details where order_no in (
-        select order_no from sales_order where client_no in (
-            select client_no from client_master where name like 'Ravi' || name like 'Basu'
-        )
-    )
-);
-
 select
     p.product_no,
     p.description,
