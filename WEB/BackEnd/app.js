6f958f79917030b165a980a10317c44dae48481e
ShambaC <38806897+ShambaC@users.noreply.github.com>
2024-06-24 21:10:43 +0530

    update backend for WEB

---
+++ WEB/BackEnd/app.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const path = require("path");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, "../Frontend")));
+
+app.get('/', (req, res) => {
+    res.sendFile("/index.html");
+});
+
+app.listen(5000, () => {
+    console.log("Server running at http://localhost:5000");
+})
