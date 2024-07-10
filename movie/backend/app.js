bfa01feb412444660e07a4acac5edcd25cf7b6f4
ShambaC <38806897+ShambaC@users.noreply.github.com>
2024-07-10 21:59:37 +0530

    Add index page

--- movie/backend/app.js
+++ movie/backend/app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const path = require("path");
 const bodyParser = require("body-parser");
 
-// const db = require("./db");
+const db = require("./db");
 
 const app = express();
 
@@ -16,19 +16,12 @@ app.use(express.static(path.join(__dirname, "../frontend")));
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '../frontend/actor_input.html'));
+  res.sendFile(path.join(__dirname, '../frontend/index.html'));
 });
 
-// app.get('/insert', async (req, res) => {
-//     try {
-//       const [rows] = await db.query('SELECT title FROM movie');
-//       res.setHeader('Content-Type', 'text/html');
-//       res.render('actor_input', { options: rows });
-//     } catch (error) {
-//       console.error(error);
-//       res.status(500).send('Error fetching options');
-//     }
-// });
+app.post('/insertActor', (req, res) => {
+    
+});
 
 app.listen(5000, () => {
   console.log("Server running at http://localhost:5000");
